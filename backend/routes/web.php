4a38b0497239c63b3a740fb4156e9273e2bee47d
Rimane_sub <rimane.o@gmail.com>
2021-09-23 19:30:33 +0900

    React 導入

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return view('App');
+})->where('any', '.*');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path')
 
 /*
  |--------------------------------------------------------------------------
@@ -6,12 +7,25 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.ts('resources/ts/App.tsx', 'public/js')
+    .react()
+    .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync('localhost')
+
+
+/**
+ * React 絶対パス
+ */
+module.exports ={
+    resolve: {
+        alias: [
+            path.resolve(__dirname,'resources/ts'),
+        ]
+    }
+}
