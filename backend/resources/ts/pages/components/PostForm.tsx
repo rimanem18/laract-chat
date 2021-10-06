e252e7cc2e1b767a634f5f035a31255d106606eb
Rimane_sub <rimane.o@gmail.com>
2021-10-06 16:02:46 +0900

    ctrl エンターキーで投稿できるように しかかり

--- backend/resources/ts/pages/components/PostForm.tsx
+++ backend/resources/ts/pages/components/PostForm.tsx
@@ -2,7 +2,13 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import { selectChatMessages } from '../../features/ChatMessagesSlice'
-import { selectUserId, selectUserName, selectUserEmail, selectUser, fetchUser } from '../../features/UserSlice'
+import {
+  selectUserId,
+  selectUserName,
+  selectUserEmail,
+  selectUser,
+  fetchUser,
+} from '../../features/UserSlice'
 import { Link } from 'react-router-dom'
 import { getContent, postMessage, selectPost } from '../../features/PostSlise'
 
@@ -22,28 +28,41 @@ const PostForm = () => {
     dispatch(getContent(e.target.value))
   }
 
+  const onSubmitKeyUpHandler = (
+    e: React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (e.code === 'Enter' || e.ctrlKey) {
+      dispatch(postMessage({ userId: user.id, content: post.content }))
+    }
+  }
+
   return (
     <>
-      {
-        user.id === 0 ?
-          <p>書き込みをするには <Link to='/auth'>ログイン</Link> してください。 </p>
-          : <>
-            <div>
-              <p>id: {user.id}</p>
-              <p>name: {user.name}</p>
-              <p>email: {user.email}</p>
-            </div>
-            <form className="form post-form" onSubmit={postMessageHandler}>
-              <textarea
-                className="post-form__input"
-                name="content"
-                value={post.content}
-                onChange={changeContentHandler}
-              ></textarea>
-              <button className="btn btn-primary mx-1 align-top" type="submit">投稿</button>
-            </form>
-          </>
-      }
+      {user.id === 0 ? (
+        <p>
+          書き込みをするには <Link to="/auth">ログイン</Link> してください。{' '}
+        </p>
+      ) : (
+        <>
+          <div>
+            <p>id: {user.id}</p>
+            <p>name: {user.name}</p>
+            <p>email: {user.email}</p>
+          </div>
+          <form className="form post-form" onSubmit={postMessageHandler}>
+            <textarea
+              className="post-form__input"
+              name="content"
+              value={post.content}
+              onChange={changeContentHandler}
+              onKeyUp={(e) => onSubmitKeyUpHandler}
+            ></textarea>
+            <button className="btn btn-primary mx-1 align-top" type="submit">
+              投稿
+            </button>
+          </form>
+        </>
+      )}
     </>
   )
 }
