3dd65c48c481f56c02ce9886a04ec8cbf2528b66
Rimane <rimane.o@gmail.com>
2021-10-18 23:31:47 +0900

    カスタムフックを使ってテストが通るように #11

--- backend/resources/ts/app/hooks.ts
+++ backend/resources/ts/app/hooks.ts
@@ -1,6 +1,56 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
+import { useCallback, useEffect, useRef } from 'react'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import {
+  fetchMessages,
+  selectChatMessagesEntities,
+  selectChatMessagesIds,
+  selectChatMessagesPromise,
+} from '../features/ChatMessagesSlice'
+import { selectPostPromise } from '../features/PostSlise'
+import type { RootState, AppDispatch } from './store'
 
 // プレーンな useDispatch と useSelector の代わりにアプリ全体で使用する
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+// ChatMessages Selector
+export function useChatMessageIds() {
+  return useAppSelector(selectChatMessagesIds)
+}
+export const useChatMessagesEntities = () => {
+  return useAppSelector(selectChatMessagesEntities)
+}
+export const useChatMessagesPromise = () => {
+  return useAppSelector(selectChatMessagesPromise)
+}
+
+// Post Selector
+export const usePostPromise = () => {
+  return useAppSelector(selectPostPromise)
+}
+
+// Render
+export const useScrollToBottom = (
+  refObject: React.MutableRefObject<HTMLElement | null>
+) => {
+  const el = refObject.current
+  if (el !== null) {
+    el.scrollTo(0, el.scrollHeight)
+  }
+}
+
+/**
+ * メッセージが何もフェッチされていないときだけfetchする
+ */
+export const useInitFetchMessages = () => {
+  const chatMessagesIds = useChatMessageIds()
+  const chatMessagesPromise = useChatMessagesPromise()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    // メッセージが何もフェッチされていないときだけ
+    if (chatMessagesIds.length === 0 || chatMessagesPromise !== 'loading') {
+      dispatch(fetchMessages())
+    }
+  }, [])
+}
--- backend/resources/ts/pages/components/Message.spec.tsx
+++ backend/resources/ts/pages/components/Message.spec.tsx
@@ -1,26 +1,67 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { fireEvent, render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Message from './Message'
+import {} from '../../app/hooks'
+import {
+  ChatMessage,
+  ChatMessagesSliceState,
+} from '../../features/ChatMessagesSlice'
+import { promiseState } from '../../app/type'
 
+// Mock の State
+const mockState: ChatMessagesSliceState = {
+  ids: ['message1', 'message2'],
+  entities: {
+    message1: {
+      id: 1,
+      name: 'hoge',
+      content: 'fugafuga',
+      created_at: '1900-01-01',
+    },
+    message2: {
+      id: 2,
+      name: 'piyo',
+      content: 'fugafuga',
+      created_at: '1900-01-01',
+    },
+  },
+  promise: 'idle',
+}
+
+// Hooks の Mock
 const mockUseAppDispatch = jest.fn()
 const mockUseAppSelector = jest.fn()
 jest.mock('../../app/hooks', () => ({
-  useAppDispatch: () => (...args: any[]) => mockUseAppDispatch(...args),
-  useAppSelector: () => (...args: any[]) => mockUseAppSelector(...args),
+  useAppDispatch:
+    () =>
+    (...args: any[]) =>
+      mockUseAppDispatch(...args),
+  useAppSelector:
+    () =>
+    (...args: any[]) =>
+      mockUseAppSelector(...args),
+  useChatMessageIds: () => mockChatMessageIds(),
+  useChatMessagesEntities: () => useChatMessagesEntitiesMock(),
+  useChatMessagesPromise: () => useChatMessagesPromiseMock(),
+  usePostPromise: () => usePostPromiseMock(),
+  useScrollToBottom: () => useScrollToBottomMock(),
+  useInitFetchMessages: () => useInitFetchMessagesMock(),
 }))
 
-const mockChatMessagesIds = ["message1", "message2"]
+// Mock の定義
+const mockChatMessageIds = jest.fn().mockReturnValue(mockState.ids)
+const useChatMessagesEntitiesMock = jest
+  .fn()
+  .mockReturnValue(mockState.entities)
+const useChatMessagesPromiseMock = jest.fn().mockReturnValue(mockState.promise)
+const usePostPromiseMock = jest.fn().mockReturnValue('idle')
+const useScrollToBottomMock = jest.fn()
+const useInitFetchMessagesMock = jest.fn()
 
-const mockScrollBottom = jest.fn()
-jest.mock('./Message'), () => ({
-  scrollToButtom: () => () => mockScrollBottom(),
-  chatMessagesIds: mockChatMessagesIds,
-})
-
+// Setup
 const setup = () => {
   const screen = render(<Message />)
-  jest.advanceTimersByTime(500)
   const scrollButton = screen.getByTestId('scroll-btn') as HTMLButtonElement
   return {
     scrollButton,
@@ -28,11 +69,11 @@ const setup = () => {
   }
 }
 
+// テスト開始
 describe('Message', () => {
   it('スクロールボタンがクリックで発火する', () => {
     const { scrollButton } = setup()
     fireEvent.click(scrollButton)
-    expect(scrollButton).toBeCalledTimes(1)
+    expect(useScrollToBottomMock).toBeCalled()
   })
-
 })
