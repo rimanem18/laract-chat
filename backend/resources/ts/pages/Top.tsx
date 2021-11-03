99e0c19fe62115f2b9dff8ec292e1086fac0919c
Rimane <rimane.o@gmail.com>
2021-11-03 15:44:47 +0900

    flex layout を調整 #29

--- backend/resources/ts/pages/Top.tsx
+++ backend/resources/ts/pages/Top.tsx
@@ -15,11 +15,11 @@ const Top = () => {
       ) : (
         <>
           <h1>TopPage</h1>
-          <div className="row">
-            <div className="col-md-3">
+          <div className="flex">
+            <div className="flex__item--3">
               <Sidebar />
             </div>
-            <div className="col-md-9">
+            <div className="flex__item--9">
               <Message />
               <PostForm />
             </div>
