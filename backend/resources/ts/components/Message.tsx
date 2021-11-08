85a0db26fa73897a5f43d6418870593f1fbd7250
Rimane_sub <rimane.o@gmail.com>
2021-11-08 14:42:00 +0900

    メッセージの名前横にカラーアイコンが表示されるように #29

--- backend/resources/ts/components/Message.tsx
+++ backend/resources/ts/components/Message.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Box } from '@mui/material'
+import { Box, Avatar, Grid } from '@mui/material'
 import {
   useChatMessageIds,
   useChatMessagesEntities,
@@ -8,6 +8,7 @@ import {
   useParamGroupId,
   useScrollToBottom,
   useUpdateMessages,
+  useStringToColor,
 } from '../app/hooks'
 import EditGroupModal from './EditGroupModal'
 
@@ -39,14 +40,12 @@ const Message = () => {
 
   return (
     <>
-      <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
-        <h2 className="h2">
-          {groupsEntities[`group${groupId}`]
-            ? groupsEntities[`group${groupId}`].name
-            : ''}
-        </h2>
+      <h2 className="h2">
+        {groupsEntities[`group${groupId}`]
+          ? groupsEntities[`group${groupId}`].name
+          : ''}
         <EditGroupModal groupId={groupId} groupName={groupName} />
-      </Box>
+      </h2>
       <div ref={messageList} className="message">
         <p className="message__note">ここが「{groupName}」の先頭です。</p>
         {chatMessagesIds.map((id: string) =>
@@ -82,14 +81,34 @@ const MessageItem = React.memo(
     // 2個以上の改行を2個改行におさめる
     content = content.replace(/\n{2,}/g, '\n\n')
     const datetime = useFormatDate(created_at)
+    const nameColor = useStringToColor(name)
 
+    function stringToColor(string: string) {
+      let hash = 0
+      let i
+
+      /* eslint-disable no-bitwise */
+      for (i = 0; i < string.length; i += 1) {
+        hash = string.charCodeAt(i) + ((hash << 5) - hash)
+      }
+
+      let color = '#'
+
+      for (i = 0; i < 3; i += 1) {
+        const value = (hash >> (i * 8)) & 0xff
+        color += `00${value.toString(16)}`.substr(-2)
+      }
+      /* eslint-enable no-bitwise */
+
+      return color
+    }
     return (
       <>
         <div className="message__item">
-          <div>
-            <strong className="mr-1">{name}</strong>
-            <small>{datetime}</small>
-          </div>
+          <Grid container>
+            <Avatar title={name} sx={{ bgcolor: nameColor, m: 1 }}></Avatar>
+            <p>{name}</p>
+          </Grid>
           <p>
             {content.split('\n').map((str, index) => (
               <React.Fragment key={index}>
--- backend/resources/ts/components/Sidebar.tsx
+++ backend/resources/ts/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import { Avatar, Button, Grid, Box } from '@mui/material'
 import LogoutIcon from '@mui/icons-material/Logout'
 import {
   useAppDispatch,
+  useStringToColor,
   useUserEmail,
   useUserId,
   useUserName,
@@ -15,48 +16,17 @@ const Sidebar = () => {
   const userName = useUserName()
   const userEmail = useUserEmail()
   const dispatch = useAppDispatch()
+  const nameColor = useStringToColor(userName)
 
   const logoutHandler = () => {
     dispatch(logout())
-  }
-
-  function stringToColor(string: string) {
-    let hash = 0
-    let i
-
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash)
-    }
-
-    let color = '#'
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff
-      color += `00${value.toString(16)}`.substr(-2)
-    }
-    /* eslint-enable no-bitwise */
-
-    return color
-  }
-
-  function stringAvatar(name: string) {
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
-    }
   }
 
   return (
     <>
       <Grid container sx={{ my: 2 }}>
         <Grid container>
-          <Avatar
-            title={userName}
-            sx={{ bgcolor: stringToColor(userName), m: 1 }}
-          ></Avatar>
+          <Avatar title={userName} sx={{ bgcolor: nameColor, m: 1 }}></Avatar>
           <p>{userName}</p>
         </Grid>
         <Grid container>
