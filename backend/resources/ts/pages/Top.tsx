e5bbc3bb21fcb4d6dc27504d00699a934c1fb727
Rimane_sub <rimane.o@gmail.com>
2021-11-15 14:47:23 +0900

    PC幅とスマホ幅でコンポーネント出し分け #52

--- backend/resources/ts/pages/Top.tsx
+++ backend/resources/ts/pages/Top.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Box, IconButton, SwipeableDrawer } from '@mui/material'
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted'
 import { Redirect } from 'react-router-dom'
@@ -10,9 +10,10 @@ import zIndex from '@mui/material/styles/zIndex'
 
 const Top = () => {
   const userId = useUserId()
+  const [width, setWidth] = useState<number>(768)
   const [isOpen, setIsOpen] = useState(false)
 
-  const toggleDrawer =
+  const toggleDrawer = useCallback(
     (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
       if (
         event &&
@@ -24,7 +25,20 @@ const Top = () => {
       }
 
       setIsOpen(open)
-    }
+    },
+    []
+  )
+
+  useEffect(() => {
+    window.addEventListener(`resize`, updateWidth, {
+      capture: false,
+      passive: true,
+    })
+    return () => window.removeEventListener(`resize`, updateWidth)
+  })
+  const updateWidth = () => {
+    setWidth(window.innerWidth)
+  }
 
   return (
     <>
@@ -32,31 +46,64 @@ const Top = () => {
         <Redirect to="/login" />
       ) : (
         <>
-          <IconButton
-            sx={{ position: 'absolute', top: 0, right: 0 }}
-            onClick={toggleDrawer(!isOpen)}
-          >
-            <FormatListBulletedIcon />
-          </IconButton>
-          <div className="flex">
-            <SwipeableDrawer
-              open={isOpen}
-              onClose={toggleDrawer(false)}
-              onOpen={toggleDrawer(true)}
-            >
-              <Box className="sidebar">
-                <Sidebar />
-              </Box>
-            </SwipeableDrawer>
-            <Box className="main">
-              <Message />
-              <PostForm />
-            </Box>
-          </div>
+          {width >= 768 ? (
+            <PcView />
+          ) : (
+            <MobileView isOpen={isOpen} toggleDrawer={toggleDrawer} />
+          )}
         </>
       )}
     </>
   )
 }
-
 export default React.memo(Top)
+
+const PcView = () => {
+  return (
+    <>
+      <div className="flex">
+        <Box className="sidebar">
+          <Sidebar />
+        </Box>
+        <Box className="main">
+          <Message />
+          <PostForm />
+        </Box>
+      </div>
+    </>
+  )
+}
+
+type MobileViewProps = {
+  isOpen: boolean
+  toggleDrawer: (
+    open: boolean
+  ) => (event: React.KeyboardEvent | React.MouseEvent) => void
+}
+const MobileView = ({ isOpen, toggleDrawer }: MobileViewProps) => {
+  return (
+    <>
+      <IconButton
+        sx={{ position: 'absolute', top: 0, right: 0 }}
+        onClick={toggleDrawer(!isOpen)}
+      >
+        <FormatListBulletedIcon />
+      </IconButton>
+      <div className="flex">
+        <SwipeableDrawer
+          open={isOpen}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+        >
+          <Box className="sidebar">
+            <Sidebar />
+          </Box>
+        </SwipeableDrawer>
+        <Box className="main">
+          <Message />
+          <PostForm />
+        </Box>
+      </div>
+    </>
+  )
+}
