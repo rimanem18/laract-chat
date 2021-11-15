5c062007223c85b1d28dd742d32816c12f7273b0
Rimane_sub <rimane.o@gmail.com>
2021-11-15 14:20:20 +0900

    サイドバーをトグルできるように #52

--- backend/resources/ts/pages/Top.tsx
+++ backend/resources/ts/pages/Top.tsx
@@ -1,28 +1,59 @@
-import { Box, Grid } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, IconButton, SwipeableDrawer } from '@mui/material'
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted'
 import { Redirect } from 'react-router-dom'
 import { useUserId } from '../app/hooks'
 import Message from '../components/Message'
 import PostForm from '../components/PostForm'
 import Sidebar from '../components/Sidebar'
+import zIndex from '@mui/material/styles/zIndex'
 
 const Top = () => {
   const userId = useUserId()
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return
+      }
+
+      setIsOpen(open)
+    }
 
   return (
     <>
       {userId === 0 ? (
         <Redirect to="/login" />
       ) : (
-        <div className="flex">
-          <Box className="sidebar">
-            <Sidebar />
-          </Box>
-          <Box className="main">
-            <Message />
-            <PostForm />
-          </Box>
-        </div>
+        <>
+          <IconButton
+            sx={{ position: 'absolute', top: 0, right: 0 }}
+            onClick={toggleDrawer(!isOpen)}
+          >
+            <FormatListBulletedIcon />
+          </IconButton>
+          <div className="flex">
+            <SwipeableDrawer
+              open={isOpen}
+              onClose={toggleDrawer(false)}
+              onOpen={toggleDrawer(true)}
+            >
+              <Box className="sidebar">
+                <Sidebar />
+              </Box>
+            </SwipeableDrawer>
+            <Box className="main">
+              <Message />
+              <PostForm />
+            </Box>
+          </div>
+        </>
       )}
     </>
   )
