5e478a51e47c90695b87d585e7bc768700d796a6
Rimane <rimane.o@gmail.com>
2021-11-16 18:45:08 +0900

    Menu Redux のテストを実装 #52

---
+++ backend/resources/ts/__tests__/slices/MenuSlice.spec.ts
@@ -0,0 +1,27 @@
+import { menuSlice, MenuState, toggleMenuOpen } from '../../slices/MenuSlice'
+
+const initialState: MenuState = {
+  isOpen: false,
+}
+
+describe('menuSlice', () => {
+  describe('reducer', () => {
+    it('toggleMenuOpen false', () => {
+      const action = {
+        type: toggleMenuOpen.type,
+        payload: false,
+      }
+      const state = menuSlice.reducer(initialState, action)
+      expect(state.isOpen).toBe(false)
+    })
+  })
+
+  it('toggleMenuOpen true', () => {
+    const action = {
+      type: toggleMenuOpen.type,
+      payload: true,
+    }
+    const state = menuSlice.reducer(initialState, action)
+    expect(state.isOpen).toBe(true)
+  })
+})
