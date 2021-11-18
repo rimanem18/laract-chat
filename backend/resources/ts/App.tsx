20ff12b737650953d5259250ef79f9ac899125bb
Rimane_sub <rimane.o@gmail.com>
2021-11-18 11:14:49 +0900

    authSlice にし忘れていた部分を修正 #64

--- backend/resources/ts/App.tsx
+++ backend/resources/ts/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import {
   useAppDispatch,
+  useAuthState,
   useFetchGroups,
-  useGroupsState,
   useInitFetchMessages,
 } from './app/hooks'
 import { fetchUser } from './slices/UserSlice'
@@ -16,8 +16,7 @@ import Register from './pages/Register'
 import AuthRedirect from './components/AuthRedirect'
 
 const App = () => {
-  const authPromise = useAuthPromise()
-  const { groupIds } = useGroupsState()
+  const { authPromise } = useAuthState()
   const dispatch = useAppDispatch()
 
   // レンダリング時にログインしているか判定
