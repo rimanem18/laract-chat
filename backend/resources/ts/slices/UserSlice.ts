d63c79db624f4e955f513d661fe14eeba5e6c0b7
Rimane_sub <rimane.o@gmail.com>
2021-11-22 14:26:49 +0900

    role をサーバー取得する処理を実装 #32

--- backend/resources/ts/slices/UserSlice.ts
+++ backend/resources/ts/slices/UserSlice.ts
@@ -2,13 +2,21 @@ import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../app/store'
 import axios from 'axios'
 import { PromiseState } from '../app/type'
+import { shallowEqual } from 'react-redux'
 
 // 型定義
 export interface UserState {
   id: number
   name: string
   email: string
+  role_ids: string[]
+  role_entities: Record<string, Role>
   promise: PromiseState
+}
+
+type Role = {
+  id: number
+  name: string
 }
 
 // 初期値
@@ -16,6 +24,13 @@ const initialState: UserState = {
   id: 0,
   name: '',
   email: '',
+  role_ids: ['role0'],
+  role_entities: {
+    role0: {
+      id: 0,
+      name: '',
+    },
+  },
   promise: 'idle',
 }
 
@@ -23,6 +38,15 @@ export const fetchUser = createAsyncThunk('user/fetchUser', async () => {
   const response = await axios.get('/api/user')
   return response.data
 })
+export const fetchRole = createAsyncThunk(
+  'user/fetchRole',
+  async ({ userId }: { userId: number }, _thunkApi) => {
+    const response = await axios.post('/api/role', {
+      userId: userId,
+    })
+    return response.data
+  }
+)
 
 export const userSlice = createSlice({
   name: 'user',
@@ -49,6 +73,23 @@ export const userSlice = createSlice({
         state.name = ''
         state.email = ''
         state.promise = 'rejected'
+      })
+
+      // fetchRole
+      .addCase(fetchRole.fulfilled, (state, action: PayloadAction<Role[]>) => {
+        const roles = action.payload
+        state.promise = 'idle'
+
+        // Slice と差がなければ帰る
+        if (shallowEqual(roles, state.role_entities)) {
+          return
+        }
+
+        state.role_ids = roles.map((role) => `role${role.id.toString()}`)
+
+        roles.forEach((role) => {
+          state.role_entities[`role${role.id.toString()}`] = role
+        })
       })
   },
 })
