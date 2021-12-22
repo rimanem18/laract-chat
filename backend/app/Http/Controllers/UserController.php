fd1284b80f0a4d25390bb27e1c90fdf001236007
Rimane <rimane.o@gmail.com>
2021-12-22 18:50:51 +0900

    User情報をIDから取得できるURLを用意 #74

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Models\User;
+use App\Models\RoleUser;
+use \Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    /**
+     * IDからユーザー情報を取得
+     *
+     * @param Request $request->groupIds
+     * @return JsonResponse
+     */
+    public function getUserById(Request $request): JsonResponse
+    {
+        $user_id = $request->userId;
+
+        $user =
+       User::where('users.id', $user_id)
+      ->select(
+          'id',
+          'name',
+          'email',
+      )
+      ->get();
+
+        $role_user =
+      RoleUser::join('users', 'role_user.user_id', '=', 'users.id')
+      ->where('users.id', $user_id)
+      ->select(
+          'user_id',
+          "role_id"
+      )
+      ->get();
+
+        return Response()->json([
+        'user'=>$user,
+        'role_user'=> $role_user
+      ], Response::HTTP_OK);
+    }
+}
