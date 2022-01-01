151574a766d74f94f5a466da61647da8fa6520a6
Rimane <rimane.o@gmail.com>
2022-01-01 16:28:26 +0900

    もとのコードを削除 #73

--- backend/resources/ts/components/Message.tsx
+++ backend/resources/ts/components/Message.tsx
@@ -24,75 +24,7 @@ import {
   messageRoleColorSelector,
 } from '../selectors/ChatMessagesSelector'
 
-const Message = () => {
-  return <MessageListContainer />
-
-  return (
-    <>
-      <GroupName
-        id={groupId ? groupId : ''}
-        name={groupName}
-        roleIds={roleIds}
-      />
-      <Box
-        sx={{
-          '&::-webkit-scrollbar': {
-            width: 2,
-            borderRadius: 5,
-          },
-          '&::-webkit-scrollbar-track': {
-            boxShadow: `inset 0 0 6px rgba(0, 0, 0, 0.3)`,
-          },
-          '&::-webkit-scrollbar-thumb': {
-            backgroundColor: 'rgba(0, 0, 0, 0.3)',
-            outline: `1px solid rgba(0, 0, 0, 0.3)`,
-          },
-
-          height: '70vh',
-          overflowY: 'scroll',
-          overflow: 'none',
-        }}
-        ref={messageList}
-      >
-        <p className="message__note">ここが「{groupName}」の先頭です。</p>
-        {chatMessagesState.messages.allIds.map((id: string) => {
-          const entity = chatMessagesState.messages.byId[id]
-
-          // ロール情報を取得
-          let role = rolesState.roles.byId[entity.roles[0]]
-
-          if (role === undefined) {
-            // ロールを持っていない場合は無難なデータを作って渡す
-            role = {
-              id: 0,
-              name: '',
-              color: '#333333',
-            }
-          }
-
-          // 2個以上の改行を2個改行におさめる
-          const content = entity.content.replace(/\n{2,}/g, '\n\n')
-
-          // グループID が一致しているものだけ出力する
-          if (Number(groupId) === entity.group_id) {
-            return (
-              <MessageItem
-                key={id}
-                name={entity.name}
-                roleColor={role.color}
-                roleName={role.name}
-                content={content}
-                created_at={entity.created_at}
-              />
-            )
-          }
-        })}
-        <ScrollButton refObject={messageList} />
-      </Box>
-    </>
-  )
-}
-
+const Message = () => <MessageListContainer />
 export default React.memo(Message)
 
 /**
@@ -113,50 +45,6 @@ const GroupName = React.memo(({ id, name, roleIds }: GroupNameProps) => {
   )
 })
 
-type MessageItemProps = {
-  name: string
-  roleName?: string
-  roleColor?: string
-  content: string
-  created_at: string
-}
-const MessageItem = React.memo(
-  ({ name, roleColor, roleName, content, created_at }: MessageItemProps) => {
-    const datetime = useFormatDate(created_at)
-
-    return (
-      <>
-        <div className="message__item">
-          <Grid container>
-            <StringAvatar name={name}></StringAvatar>
-            <Box sx={{ m: 1 }}>
-              <Box
-                sx={{
-                  color: roleColor,
-                  fontWeight: 'bold',
-                  display: 'block',
-                  fontSize: '80%',
-                }}
-              >
-                {name}
-              </Box>
-              <Box sx={{ display: 'block', fontSize: '80%' }}>{datetime}</Box>
-            </Box>
-          </Grid>
-          <p>
-            {content.split('\n').map((str, index) => (
-              <React.Fragment key={index}>
-                {str}
-                <br />
-              </React.Fragment>
-            ))}
-          </p>
-        </div>
-      </>
-    )
-  }
-)
-
 type ScrollButtonProps = {
   refObject: React.MutableRefObject<HTMLElement | null>
 }
@@ -176,6 +64,7 @@ const ScrollButton = React.memo(({ refObject }: ScrollButtonProps) => {
   )
 })
 
+// メッセージ一つ
 type MessageBlockProps = {
   id: string
   name: string
@@ -216,6 +105,7 @@ const MessageBlock = React.memo(
   }
 )
 
+// メッセージ一覧
 type MessageBlockListProps = {
   messageIds: string[]
   groupId: string
@@ -268,6 +158,9 @@ const MessageBlockList = ({
   )
 }
 
+/**
+ * Container
+ */
 type MessageBlockContainerProps = {
   id: string
   paramGroupId: string
